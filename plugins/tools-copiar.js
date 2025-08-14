c850a404fa6e91e9ebe958c184d03dba524852ac
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-14 12:08:31 -0300

    Update tools-copiar.js

--- plugins/tools-copiar.js
+++ plugins/tools-copiar.js
@@ -1,19 +1,1 @@
-let handler = async (m, { args}) => {
-  let code = args[0]
-  if (!code ||!/^\d{4}-\d{4}$/.test(code)) {
-    return m.reply('⚠️ No se recibió un código válido.')
-}
 
-  await m.reply(`📋 Cᴏ́ᴅɪɢᴏ ᴄᴏᴘɪᴀᴅᴏ:\n\n*${code}*`)
-
-  // Opcional: enviar como sticker (requiere sticker lib)
-  // await conn.sendImageAsSticker(m.chat, `https://fakeimg.pl/300x100/?text=${code}`, m)
-
-  // Opcional: guardar en base de datos
-  global.db.data.users[m.sender].lastCode = code
-}
-
-handler.help = ['copiar']
-handler.tags = ['tools']
-handler.command = ['copiar']
-export default handler
